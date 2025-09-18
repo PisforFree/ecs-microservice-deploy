088d4a113350c989c98abd4a90440a393c9359ea
Parris Brantley <brantleyparris@yahoo.com>
2025-09-18 02:21:31 -0400

    Add these 3 files to the test/plan-pr branch

--- ecs-microservices-deploy/infra/terraform/variables.tf
+++ ecs-microservices-deploy/infra/terraform/variables.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "region" {
   type    = string
   default = "us-east-2"
@@ -19,3 +20,26 @@ variable "image_uri" {
   description = "ECR image URI for the ECS task definition"
   type        = string
 }
+=======
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "project_prefix" {
+  type    = string
+  default = "micro"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+# trigger workflow
+# pr-trigger
+
+variable "image_uri" {
+  description = "ECR image URI for the ECS task definition"
+  type        = string
+}
+>>>>>>> 946af65 (Add these 3 files to the test/plan-pr branch)
