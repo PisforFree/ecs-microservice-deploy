14a7610fa34e79a99c2f8e707f28a8b4957004f0
Parris Brantley <brantleyparris@yahoo.com>
2025-09-18 22:55:17 -0400

    Add ECS service autoscaling configuration and variables

--- ecs-microservices-deploy/infra/terraform/variables.tf
+++ ecs-microservices-deploy/infra/terraform/variables.tf
@@ -17,3 +17,45 @@ variable "image_uri" {
   description = "ECR image URI (prefer a pinned digest)"
   type        = string
 }
+
+# ECS identifiers for autoscaling
+variable "ecs_cluster_name" {
+  description = "Exact ECS cluster name"
+  type        = string
+}
+
+variable "ecs_service_name" {
+  description = "Exact ECS service name"
+  type        = string
+}
+
+# Scaling settings
+variable "min_task_count" {
+  description = "Minimum number of tasks"
+  type        = number
+  default     = 1
+}
+
+variable "max_task_count" {
+  description = "Maximum number of tasks"
+  type        = number
+  default     = 4
+}
+
+variable "cpu_target_percent" {
+  description = "Target average CPU utilization for the service"
+  type        = number
+  default     = 50
+}
+
+variable "scale_in_cooldown" {
+  description = "Seconds to wait after a scale in activity"
+  type        = number
+  default     = 120
+}
+
+variable "scale_out_cooldown" {
+  description = "Seconds to wait after a scale out activity"
+  type        = number
+  default     = 60
+}
