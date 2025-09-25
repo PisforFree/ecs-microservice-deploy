75313f6f78184bbf37ac8048a5002c735d08bdb6
Parris Brantley <brantleyparris@yahoo.com>
2025-09-25 13:54:17 -0400

    chore(tf): fmt backend and modules

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket               = "tfstate-brant-202509221842"
-    key                  = "terraform.tfstate"   # state file name only
-    workspace_key_prefix = "envs"               # workspace folder prefix
+    key                  = "terraform.tfstate" # state file name only
+    workspace_key_prefix = "envs"              # workspace folder prefix
     region               = "us-east-2"
     dynamodb_table       = "tf-locks"
     encrypt              = true
