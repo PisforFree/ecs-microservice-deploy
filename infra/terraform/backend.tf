0d3ddbe560e52c461fb55e126e7366904996b4dd
Parris Brantley <brantleyparris@yahoo.com>
2025-09-25 13:16:20 -0400

    backend: use workspace_key_prefix=envs and key=terraform.tfstate

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-brant-202509221842"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
+    bucket               = "tfstate-brant-202509221842"
+    key                  = "terraform.tfstate"   # state file name only
+    workspace_key_prefix = "envs"               # workspace folder prefix
+    region               = "us-east-2"
+    dynamodb_table       = "tf-locks"
+    encrypt              = true
   }
 }
