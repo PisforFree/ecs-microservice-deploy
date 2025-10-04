4bd6916da199f4eb9a8d19dabdc37012a25d0027
PisforFree <brantleyparris@yahoo.com>
2025-10-04 12:57:18 -0400

    Chore/fix rtb subnets (#34)
    * networking: add default route in private RT to NAT for ECS egress
    
    * networking: add RTB to both subnets; ensure default route on RTB

--- infra/terraform/networking/private_routes.tf
+++ infra/terraform/networking/private_routes.tf
@@ -4,3 +4,15 @@ resource "aws_route" "private_default_to_nat" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = "nat-06063c10377fba6a0"
 }
+
+# Ensure both private subnets use THIS private RTB
+resource "aws_route_table_association" "priv1" {
+  subnet_id      = "subnet-06c96b155527807d1"
+  route_table_id = "rtb-0cfd187c223fc7632"
+}
+
+resource "aws_route_table_association" "priv2" {
+  subnet_id      = "subnet-0cb937fb5a2ff2457"
+  route_table_id = "rtb-0cfd187c223fc7632"
+}
+
