94fcaf1f256ab13bf50309323c462ef053ddff45
Parris Brantley <brantleyparris@yahoo.com>
2025-10-04 19:06:22 -0400

    chore: remove duplicate ECS log groups from ecs module (use infra/terraform/awslogs.tf only)

--- infra/terraform/ecs/logs.tf
+++
@@ -1,9 +0,0 @@
-# infra/terraform/logs.tf  (new file at the same level as your other resources)
-resource "aws_cloudwatch_log_group" "ecs" {
-  name              = "/ecs/${var.project_prefix}-${var.env}" # -> /ecs/micro-dev
-  retention_in_days = 14
-  tags = {
-    Project = var.project_prefix
-    Env     = var.env
-  }
-}
